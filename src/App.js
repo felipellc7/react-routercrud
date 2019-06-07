1a6cd3ec787d827d320b211e86c21c5b15c8a58e
Felipe Llerena <llerenafelipe7@gmail.com>
2019-06-07 13:37:59 -0500

    Create Header Component and set navigation

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Products from './components/Products';
 import Product from './components/Product';
 import AddProduct from './components/AddProduct';
 import EditProduct from './components/EditProduct';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/products/new" component={AddProduct} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/products/:id" component={Product} />
-        <Route exact path="/products/edit/:id" component={EditProduct} />
-      </Switch>
+      <Header/>
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/new-product" component={AddProduct} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/products/:id" component={Product} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </main>
     </Router>
   );
 }
