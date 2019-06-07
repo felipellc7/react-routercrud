410e10e88795d8d161c61c11cff7abb7da3af5f7
Felipe Llerena <llerenafelipe7@gmail.com>
2019-06-07 16:14:13 -0500

    validate form

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
+import Error from './Error';
 
 const AddProduct = () => {
   const [saucerName, setSaucerName] = useState('');
   const [saucerPrice, setSaucerPrice] = useState('');
   const [category, setCategory] = useState('');
+  const [error, setError] = useState(false);
 
   const readRadioValue = e => {
     setCategory(e.target.value)
   }
 
+  const addProduct = e => {
+    e.preventDefault();
+    if (saucerName === '' || saucerPrice === '' || category === '') {
+      setError(true);
+      return;
+    }
+    setError(false);
+  }
+
   return (
     <div className="col-md-8 mx-auto ">
             <h1 className="text-center">Add New Saucer</h1>
-
+            {error ? <Error message="All fields are required!" /> : null}
             <form
                 className="mt-5"
+                onSubmit={addProduct}
             >
                 <div className="form-group">
                     <label>Saucer Name</label>
