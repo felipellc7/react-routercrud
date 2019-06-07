be8db3567080b79470fbf2d6d8a20e82d0697cca
Felipe Llerena <llerenafelipe7@gmail.com>
2019-06-07 12:34:39 -0500

    Create Routes and Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Products from './components/Products';
+import Product from './components/Product';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/products/new" component={AddProduct} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:id" component={Product} />
+        <Route exact path="/products/edit/:id" component={EditProduct} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/EditProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditProduct = () => {
+  return (
+    <div>
+      <h1 className="text-center">Edit Product</h1>
+    </div>
+  )
+}
+
+export default EditProduct;
